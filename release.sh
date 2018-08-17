83460eba7c967ba373f87302573733eeff87b29e
Karl Ranna <karl@karlranna.com>
2018-08-17 16:23:14 +0300

    Add release script
    Adds a release script that generates a new `draft` release to GitHub repository
    via the API. Release notes are generated from the commits since the last
    tag.

---
+++ release.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -e
+# avoid issues with relative paths
+cd $(dirname $0)
+
+if [ -z "$(eval "echo \$GH_TOKEN")" ]; then
+  echo "Missing environment variable GH_TOKEN."
+  exit 1
+fi
+
+TEMP_DATA_JSON="release.json"
+PACKAGE="package.json"
+GITHUB_API_ENDPOINT="https://api.github.com/repos/lightninglabs/lightning-app/releases"
+
+if [ $# -ne 1 ]; then
+    echo You must provide only a release version!
+    echo Current release: `sed -n 's/.*"version": "\(.*\)".*/\1/p' ${PACKAGE}`
+    exit 1
+fi
+RELEASE=$1
+
+# fetch all current tags
+git fetch --tags
+
+# generate boilerplate release notes from commits
+RELEASE_NOTES="$(git log $(git describe --tags --abbrev=0)..HEAD --pretty='* %s (%h)\n')"
+
+# ensure temp file is empty
+rm -f ${TEMP_DATA_JSON}
+
+DATA_JSON='{"tag_name": "'"$RELEASE"'", "name": "'"$RELEASE"'", "body": "'"$RELEASE_NOTES"'", "prerelease": true, "draft": true}'
+
+echo "${DATA_JSON}" >> ${TEMP_DATA_JSON}
+
+curl -H "Content-Type: application/json" \
+  -H "Authorization: token $GH_TOKEN" \
+  --data @$TEMP_DATA_JSON \
+  $GITHUB_API_ENDPOINT
+
+# cleanup
+rm -f ${TEMP_DATA_JSON}
